a73783f597973fb8266ace7f8c52a06e149599d7
Adam Gall <adam@decentlabs.io>
2020-01-24 16:31:02 -0500

    Update the startup logs to show the fee

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ dotenv.config();
 
 const go = () => {
   console.log();
-  console.log('Welcome to the Newkey generator!');
+  console.log('Welcome to the Ice Cubed Wallet generator!');
   console.log('This app uses a static address to receive BCH.');
-  console.log('A small fee will be collected from any received BCH,');
+  console.log(`A small fee (${process.env.FEE_AMOUNT_SATS} sats) will be collected from any received BCH,`);
   console.log('and the remainder will be sent to a fresh wallet.');
 
   const staticKey = makeNewKey(process.env.STATIC_ADDRESS_ENTROPY);
@@ -16,9 +16,12 @@ const go = () => {
   return generateQR(staticKey).then(qr => {
     console.log();
     console.log(staticKey);
+    console.log();
     console.log(qr);
     console.log();
     console.log('Send BCH, and receive a new address with that BCH')
+    console.log();
+    console.log("----------------------------------------------------------------------")
     console.log();
   });
 }
